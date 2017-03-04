5b4531bd815f3a3adf0e0763f4f944c31e8ad729
Danielle Closs <anastasiavixen@gmail.com>
2017-03-04 15:07:59 -0500

    updating code formatting

--- src/App.jsx
+++ src/App.jsx
@@ -16,15 +16,15 @@ class App extends React.Component {
     console.log("Year Selected: " + year);
   }
 
-	render () {
-		return (
-			<section>
-				<h1>Welcome to <span>WE MAKE WIDGETS&reg;</span> online!</h1>
-				<p>For all your <span>WIDGET&reg;</span> needs, come to us first!  We have a proven track record, and can fulfill your <span>WIDGET&reg;</span> needs, under budget with fast delivery!</p>
-				<InputForm captureYear={this.captureYear}/>
-			</section>
-		);
-	}
+  render () {
+    return (
+      <section>
+        <h1>Welcome to <span>WE MAKE WIDGETS&reg;</span> online!</h1>
+        <p>For all your <span>WIDGET&reg;</span> needs, come to us first!  We have a proven track record, and can fulfill your <span>WIDGET&reg;</span> needs, under budget with fast delivery!</p>
+        <InputForm captureYear={this.captureYear}/>
+      </section>
+    );
+  }
 }
 
 render(<App/>, document.getElementById('mount-point'));
